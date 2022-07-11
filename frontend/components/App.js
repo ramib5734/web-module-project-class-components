3734f22c1a24c07207ce9800da3545fad1041936
unknown <ramib1207@gmail.com>
2022-07-11 15:00:56 -0400

    App Shell Created

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,18 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1> TODO LIST </h1>
+        <ul>
+          <li>Make Lunch</li>
+          <li>Walk the Cat</li>
+          <li>Wash your shoes</li>
+        </ul>
+
+        <form>
+          <input type='text' value='Enter a Todo'/>
+          <input type='submit' value='Add Todo'/>
+        </form>
+        <button> Clear Todos </button>
       </div>
     )
   }
